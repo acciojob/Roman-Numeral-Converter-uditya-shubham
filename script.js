340541b58f30a986ca3469a3411ed1f2c2227b09
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-30 00:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ["M", 1000],
-    1: ["D", 500],
-    2: ["C", 100],
-    3: ["L", 50],
-    4: ["X", 10],
-    6: ["I", 1],
-  };
-
-  //your code here
+  const symbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
   let result = '';
     
@@ -20,12 +25,11 @@ function convertToRoman(num) {
   }
   
   return result;
-
-
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36)); // Output: XXXVI
 
 // do not edit below this line
 module.exports = convertToRoman;
